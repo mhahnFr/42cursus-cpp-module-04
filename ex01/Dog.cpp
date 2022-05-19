c7da137da215187a5ebb2be5bab71f8efb464c3d
mhahn <mhahn@student.42heilbronn.de>
2022-05-19 16:06:36 +0200

    Wrote ex01

---
+++ ex01/Dog.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Manuel Hahn on 5/19/22.
+//
+
+#include <iostream>
+#include "Dog.h"
+
+Dog::Dog(): Animal(), brain(new Brain) {
+    type = "Dog";
+    std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other): Animal(other), brain(new Brain(*other.getBrain())) {
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog::~Dog() {
+    std::cout << "Dog destroyed" << std::endl;
+    delete brain;
+}
+
+void Dog::makeSound() const {
+    std::cout << "Bark bark!" << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &other) {
+    std::cout << "Dog assignment operator called" << std::endl;
+    if (&other != this) {
+        type = other.getType();
+        delete brain;
+        brain = new Brain(*other.getBrain());
+    }
+    return *this;
+}
+
+Brain *Dog::getBrain() const {
+    return brain;
+}
