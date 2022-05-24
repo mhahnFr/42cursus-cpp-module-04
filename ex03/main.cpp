3d5ff652537c024dc00775b44a38b26b8709921e
mhahn <mhahn@student.42heilbronn.de>
2022-05-24 14:55:14 +0200

    Added more tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -8,6 +8,45 @@
 #include "Cure.h"
 #include "Character.h"
 
+void main2() {
+    IMateriaSource * src = new MateriaSource();
+    src->learnMateria(new Cure());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    AMateria * tmp = new Ice();
+    src->learnMateria(tmp);
+    delete tmp;
+
+    ICharacter* me = new Character("me");
+    ICharacter* bob = new Character("bob");
+    me->equip(src->createMateria("ice"));
+    AMateria* cure = src->createMateria("cure");
+    me->equip(cure);
+    me->equip(src->createMateria("cure"));
+    me->equip(src->createMateria("ice"));
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    me->unequip(1);
+    me->equip(tmp);
+    delete cure;
+
+    me->use(4, *bob);
+    me->use(3, *bob);
+    me->use(2, *bob);
+    me->use(1, *bob);
+    me->use(0, *bob);
+    me->use(-1, *bob);
+
+    bob = me;
+
+    me->use(2, *bob);
+
+    delete me;
+    delete bob;
+    delete src;
+}
+
 int main() {
     IMateriaSource * src = new MateriaSource();
     src->learnMateria(new Ice());
@@ -29,5 +68,6 @@ int main() {
     delete bob;
     delete src;
 
+    main2();
     return 0;
 }
